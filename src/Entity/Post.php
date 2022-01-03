5843031fed931868bc688c0237c7486e7653d09f
SylvainLabomode <sylvain@labomode.com>
2022-01-03 09:08:52 +0100

    updt tuto

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,14 +5,37 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups" : {"read:collection"}},
+ *     itemOperations={
+ *        "put" : {
+ *           "denormalization_context" : {
+ *               "groups" : {
+ *                   "put:Post"
+ *               }
+ *           }
+ *        },
+ *        "delete",
+ *        "get" : {
+ *           "normalization_context" : {
+ *               "groups" : {
+ *                  "read:collection",
+ *                  "read:item",
+ *                  "read:Post"
+ *               }
+ *           }
+ *        }
+ *     }
+ * )
  */
 class Post
 {
     /**
+     * @Groups({"read:collection"})
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -20,22 +43,26 @@ class Post
     private $id;
 
     /**
+     * @Groups ({"read:collection", "put:Post"})
      * @ORM\Column(type="string", length=255)
      */
     private $title;
 
     /**
+     * @Groups ({"read:collection"})
      * @ORM\Column(type="string", length=255)
      */
     private $slug;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"read:item"})
      */
     private $content;
 
     /**
      * @ORM\Column(type="datetime_immutable")
+     * @Groups({"read:item"})
      */
     private $createdAt;
 
@@ -43,6 +70,12 @@ class Post
      * @ORM\Column(type="datetime_immutable")
      */
     private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     * @Groups({"read:item"})
+     */
+    private $category;
 
     public function getId(): ?int
     {
@@ -105,6 +138,18 @@ class Post
     public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
